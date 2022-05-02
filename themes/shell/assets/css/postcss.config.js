15cd866c10ad4c044dea96d82179036e1cdb2b97
Tom McLoughlin <thomas@springsy.co.uk>
2022-05-02 23:15:49 +0100

    attempting to make GHA work with purgecss

--- themes/shell/assets/css/postcss.config.js
+++ themes/shell/assets/css/postcss.config.js
@@ -1,8 +1,25 @@
-let tailwindConfig = process.env.HUGO_FILE_TAILWIND_CONFIG_JS || './tailwind.config.js';
-const tailwind = require('tailwindcss')(tailwindConfig);
-const autoprefixer = require('autoprefixer');
+const themeDir = __dirname + '/../../';
 
-module.exports = {
-	// eslint-disable-next-line no-process-env
-	plugins: [ tailwind, ...(process.env.HUGO_ENVIRONMENT === 'production' ? [ autoprefixer ] : []) ]
-};
+const purgecss = require('@fullhuman/postcss-purgecss')({
+    // see https://gohugo.io/hugo-pipes/postprocess/#css-purging-with-postcss
+    content: [
+        './hugo_stats.json',
+        themeDir + '/hugo_stats.json',
+        'exampleSite/hugo_stats.json',
+    ],
+    safelist : [ /type/ ],
+    defaultExtractor: (content) => {
+        let els = JSON.parse(content).htmlElements;
+        return els.tags.concat(els.classes, els.ids);
+    }
+})
+
+module.exports = {    
+    plugins: [
+        require('tailwindcss')(themeDir + 'assets/css/tailwind.config.js'),
+        require('autoprefixer')({
+            path: [themeDir]
+        }),
+        ...(process.env.HUGO_ENVIRONMENT === 'production' ? [purgecss] : [])
+    ]
+}
