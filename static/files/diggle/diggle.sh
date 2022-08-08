a95ff80e1ec53b6a928ec1e40085231346bd2285
Tom McLoughlin <root@tmclo.dev>
2022-08-08 18:21:10 +0100

    update

---
+++ static/files/diggle/diggle.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+[[ -z "$1" ]] && echo -e "\e[31mUsage:\e[0m $0 example.com" && exit
+
+echo -e "\e[1m\e[31m---------------------------------------------\e[0m"
+echo -e "\e[32m   checking dns for\e[34m" $1
+echo -e "\e[1m\e[31m---------------------------------------------\e[0m"
+while read p; do
+	echo -e "\e[94m" $1 "\e[32m IN A       \e[35m" $p "\e[0m\e[95m " $(dig +short @1.1.1.1 -x $p) "\e[0m"
+done < <(dig +short @1.1.1.1 in a $1)
+
+while read p; do
+	echo -e "\e[94m" $1 "\e[32m IN AAAA    \e[35m" $p "\e[0m\e[95m " $(dig +short @1.1.1.1 -x $p) "\e[0m"
+done < <(dig +short @1.1.1.1 in aaaa $1)
+
+while read p; do
+	echo -e "\e[94m" $1 "\e[32m IN MX    \e[35m" $p "\e[0m"
+done < <(dig +short @1.1.1.1 in mx $1)
+
+while read p; do
+	echo -e "\e[94m" $1 "\e[32m IN TXT     \e[35m" $p "\e[0m"
+done < <(dig +short @1.1.1.1 in txt $1)
+
+while read p; do
+	echo -e "\e[94m" $1 "\e[32m IN SOA     \e[35m" $p "\e[0m"
+done < <(dig +short @1.1.1.1 in soa $1)
+
+echo -e "\e[1m\e[31m---------------------------------------------\e[0m"
+echo -e "\e[32m  (\e[96m\e[1mGURU\e[0m\e[32m) checking dns for\e[34m" $1
+echo -e "\e[1m\e[31m---------------------------------------------\e[0m"
+while read p; do
+        echo -e "\e[94m" $1 "\e[32m IN A       \e[35m" $p "\e[0m\e[95m " $(dig +short -x $p) "\e[0m"
+done < <(dig +short @ns1.myukcloud.com in a $1)
+
+while read p; do
+	echo -e "\e[94m" $1 "\e[32m IN AAAA    \e[35m" $p "\e[0m\e[95m " $(dig +short -x $p) "\e[0m"
+done < <(dig +short @ns1.myukcloud.com in aaaa $1)
+
+while read p; do
+        echo -e "\e[94m" $1 "\e[32m IN MX    \e[35m" $p "\e[0m"
+done < <(dig +short @ns1.myukcloud.com in mx $1)
+
+while read p; do
+        echo -e "\e[94m" $1 "\e[32m IN TXT     \e[35m" $p "\e[0m"
+done < <(dig +short @ns1.myukcloud.com in txt $1)
+
+while read p; do
+        echo -e "\e[94m" $1 "\e[32m IN SOA     \e[35m" $p "\e[0m"
+done < <(dig +short @ns1.myukcloud.com in soa $1)
+
+echo -e "\e[1m\e[31m---------------------------------------------\e[0m"
+echo -e "\e[1m\e[31m                 Name servers     \e[0m"
+echo -e "\e[1m\e[31m---------------------------------------------\e[0m"
+while read p; do
+	echo -e "\e[1m\e[34m " $p "\e[0m\e[35m(\e[95m\e[1m"$(dig +short @1.1.1.1 $p)"\e[35m)\e[0m"
+done < <(dig +short @1.1.1.1 ns $1 | sed 's/.$//')
